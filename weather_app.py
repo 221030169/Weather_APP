7fc9238fba09cf03216990e9c8ad7374d46c08a0
Priyansh Lunawat <priyanshlunawat2207@gmail.com>
2023-08-16 16:47:49 +0530

    Initial commit

---
+++ weather_app.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+import requests
+
+class WeatherApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Weather App")
+
+        self.api_key = "b330ba9274582c195c94173ad084cc2a"  # Replace with your OpenWeatherMap API key
+
+        self.city_label = tk.Label(root, text="Enter City:")
+        self.city_label.pack()
+
+        self.city_entry = tk.Entry(root)
+        self.city_entry.pack()
+
+        self.weather_label = tk.Label(root, text="")
+        self.weather_label.pack()
+
+        self.get_weather_button = tk.Button(root, text="Get Weather", command=self.get_weather)
+        self.get_weather_button.pack()
+
+    def get_weather(self):
+        city = self.city_entry.get()
+        if city:
+            try:
+                url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={self.api_key}&units=metric"
+                response = requests.get(url)
+                data = response.json()
+
+                weather = data['weather'][0]['description']
+                temperature = data['main']['temp']
+
+                weather_text = f"Weather: {weather}\nTemperature: {temperature}°C"
+                self.weather_label.config(text=weather_text)
+            except Exception as e:
+                self.weather_label.config(text="City not found!")
+
+if __name__ == "__main__":
+  root = tk.Tk()
+  weather_app = WeatherApp(root)
+  root.mainloop()
