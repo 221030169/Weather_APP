76a3484ebdd1a91db6f584bfb83ccf9122502b10
Priyansh Lunawat <142413921+221030169@users.noreply.github.com>
2023-08-17 17:10:39 +0530

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -8,16 +8,16 @@ class WeatherApp:
 
         self.api_key = "b330ba9274582c195c94173ad084cc2a"  # Replace with your OpenWeatherMap API key
 
-        self.city_label = tk.Label(root, text="Enter City:")
+        self.city_label = tk.Label(root, text="Enter City:",font=('Arial', 25))
         self.city_label.pack()
 
-        self.city_entry = tk.Entry(root)
+        self.city_entry = tk.Entry(root,bg="white", fg="black",font=('Arial', 25))
         self.city_entry.pack()
 
         self.weather_label = tk.Label(root, text="")
         self.weather_label.pack()
 
-        self.get_weather_button = tk.Button(root, text="Get Weather", command=self.get_weather)
+        self.get_weather_button = tk.Button(root, text="Get Weather",font=('Arial', 12) , command=self.get_weather)
         self.get_weather_button.pack()
 
     def get_weather(self):
@@ -32,9 +32,9 @@ class WeatherApp:
                 temperature = data['main']['temp']
 
                 weather_text = f"Weather: {weather}\nTemperature: {temperature}°C"
-                self.weather_label.config(text=weather_text)
+                self.weather_label.config(text=weather_text,font=('Arial', 20))
             except Exception as e:
-                self.weather_label.config(text="City not found!")
+                self.weather_label.config(text="City not found!",font=('Arial', 20))
 
 if __name__ == "__main__":
   root = tk.Tk()
